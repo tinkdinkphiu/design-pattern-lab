b64db580760a610bd4fdd0c6f61cdfb9bc925812
tinkdinkphiu <ltphu2153@gmail.com>
2024-03-27 08:28:02 +0700

    ui for command pattern

---
+++ command/Ex2/Ex2/Form1.Designer.cs
@@ -0,0 +1,433 @@
+﻿namespace Ex2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            panel1 = new Panel();
+            panel9 = new Panel();
+            panel8 = new Panel();
+            panel7 = new Panel();
+            panel6 = new Panel();
+            panel5 = new Panel();
+            panel4 = new Panel();
+            panel3 = new Panel();
+            panel2 = new Panel();
+            panel13 = new Panel();
+            btn_Off7 = new Button();
+            btn_Off4 = new Button();
+            btn_Off6 = new Button();
+            btn_Off1 = new Button();
+            btn_Off5 = new Button();
+            btn_Off2 = new Button();
+            btn_Off3 = new Button();
+            panel12 = new Panel();
+            btn_On7 = new Button();
+            btn_On6 = new Button();
+            btn_On5 = new Button();
+            btn_On4 = new Button();
+            btn_On3 = new Button();
+            btn_On2 = new Button();
+            btn_On1 = new Button();
+            panel11 = new Panel();
+            btn_Redo = new Button();
+            panel10 = new Panel();
+            btn_Undo = new Button();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            lightToolStripMenuItem = new ToolStripMenuItem();
+            fanToolStripMenuItem = new ToolStripMenuItem();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel13.SuspendLayout();
+            panel12.SuspendLayout();
+            panel11.SuspendLayout();
+            panel10.SuspendLayout();
+            contextMenuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(panel9);
+            panel1.Controls.Add(panel8);
+            panel1.Controls.Add(panel7);
+            panel1.Controls.Add(panel6);
+            panel1.Controls.Add(panel5);
+            panel1.Controls.Add(panel4);
+            panel1.Controls.Add(panel3);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(930, 240);
+            panel1.TabIndex = 0;
+            // 
+            // panel9
+            // 
+            panel9.BackColor = SystemColors.Control;
+            panel9.BorderStyle = BorderStyle.Fixed3D;
+            panel9.ContextMenuStrip = contextMenuStrip1;
+            panel9.Location = new Point(746, 12);
+            panel9.Name = "panel9";
+            panel9.Size = new Size(105, 222);
+            panel9.TabIndex = 6;
+            // 
+            // panel8
+            // 
+            panel8.BackColor = SystemColors.Control;
+            panel8.BorderStyle = BorderStyle.Fixed3D;
+            panel8.ContextMenuStrip = contextMenuStrip1;
+            panel8.Location = new Point(635, 12);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(105, 222);
+            panel8.TabIndex = 5;
+            // 
+            // panel7
+            // 
+            panel7.BackColor = SystemColors.Control;
+            panel7.BorderStyle = BorderStyle.Fixed3D;
+            panel7.ContextMenuStrip = contextMenuStrip1;
+            panel7.Location = new Point(524, 12);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(105, 222);
+            panel7.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = SystemColors.Control;
+            panel6.BorderStyle = BorderStyle.Fixed3D;
+            panel6.ContextMenuStrip = contextMenuStrip1;
+            panel6.Location = new Point(413, 12);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(105, 222);
+            panel6.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = SystemColors.Control;
+            panel5.BorderStyle = BorderStyle.Fixed3D;
+            panel5.ContextMenuStrip = contextMenuStrip1;
+            panel5.Location = new Point(302, 12);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(105, 222);
+            panel5.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = SystemColors.Control;
+            panel4.BorderStyle = BorderStyle.Fixed3D;
+            panel4.ContextMenuStrip = contextMenuStrip1;
+            panel4.Location = new Point(191, 12);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(105, 222);
+            panel4.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.Control;
+            panel3.BorderStyle = BorderStyle.Fixed3D;
+            panel3.ContextMenuStrip = contextMenuStrip1;
+            panel3.Location = new Point(80, 12);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(105, 222);
+            panel3.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(panel13);
+            panel2.Controls.Add(panel12);
+            panel2.Controls.Add(panel11);
+            panel2.Controls.Add(panel10);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 240);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(930, 248);
+            panel2.TabIndex = 1;
+            // 
+            // panel13
+            // 
+            panel13.Controls.Add(btn_Off7);
+            panel13.Controls.Add(btn_Off4);
+            panel13.Controls.Add(btn_Off6);
+            panel13.Controls.Add(btn_Off1);
+            panel13.Controls.Add(btn_Off5);
+            panel13.Controls.Add(btn_Off2);
+            panel13.Controls.Add(btn_Off3);
+            panel13.Location = new Point(80, 131);
+            panel13.Name = "panel13";
+            panel13.Size = new Size(771, 117);
+            panel13.TabIndex = 3;
+            // 
+            // btn_Off7
+            // 
+            btn_Off7.Location = new Point(687, 3);
+            btn_Off7.Name = "btn_Off7";
+            btn_Off7.Size = new Size(75, 111);
+            btn_Off7.TabIndex = 13;
+            btn_Off7.Text = "Off";
+            btn_Off7.UseVisualStyleBackColor = true;
+            // 
+            // btn_Off4
+            // 
+            btn_Off4.Location = new Point(348, 3);
+            btn_Off4.Name = "btn_Off4";
+            btn_Off4.Size = new Size(75, 111);
+            btn_Off4.TabIndex = 10;
+            btn_Off4.Text = "Off";
+            btn_Off4.UseVisualStyleBackColor = true;
+            // 
+            // btn_Off6
+            // 
+            btn_Off6.Location = new Point(574, 3);
+            btn_Off6.Name = "btn_Off6";
+            btn_Off6.Size = new Size(75, 111);
+            btn_Off6.TabIndex = 12;
+            btn_Off6.Text = "Off";
+            btn_Off6.UseVisualStyleBackColor = true;
+            // 
+            // btn_Off1
+            // 
+            btn_Off1.Location = new Point(9, 3);
+            btn_Off1.Name = "btn_Off1";
+            btn_Off1.Size = new Size(75, 111);
+            btn_Off1.TabIndex = 7;
+            btn_Off1.Text = "Off";
+            btn_Off1.UseVisualStyleBackColor = true;
+            // 
+            // btn_Off5
+            // 
+            btn_Off5.Location = new Point(461, 3);
+            btn_Off5.Name = "btn_Off5";
+            btn_Off5.Size = new Size(75, 111);
+            btn_Off5.TabIndex = 11;
+            btn_Off5.Text = "Off";
+            btn_Off5.UseVisualStyleBackColor = true;
+            // 
+            // btn_Off2
+            // 
+            btn_Off2.Location = new Point(122, 3);
+            btn_Off2.Name = "btn_Off2";
+            btn_Off2.Size = new Size(75, 111);
+            btn_Off2.TabIndex = 8;
+            btn_Off2.Text = "Off";
+            btn_Off2.UseVisualStyleBackColor = true;
+            // 
+            // btn_Off3
+            // 
+            btn_Off3.Location = new Point(235, 3);
+            btn_Off3.Name = "btn_Off3";
+            btn_Off3.Size = new Size(75, 111);
+            btn_Off3.TabIndex = 9;
+            btn_Off3.Text = "Off";
+            btn_Off3.UseVisualStyleBackColor = true;
+            // 
+            // panel12
+            // 
+            panel12.Controls.Add(btn_On7);
+            panel12.Controls.Add(btn_On6);
+            panel12.Controls.Add(btn_On5);
+            panel12.Controls.Add(btn_On4);
+            panel12.Controls.Add(btn_On3);
+            panel12.Controls.Add(btn_On2);
+            panel12.Controls.Add(btn_On1);
+            panel12.Location = new Point(80, 0);
+            panel12.Name = "panel12";
+            panel12.Size = new Size(771, 125);
+            panel12.TabIndex = 2;
+            // 
+            // btn_On7
+            // 
+            btn_On7.Location = new Point(687, 6);
+            btn_On7.Name = "btn_On7";
+            btn_On7.Size = new Size(75, 116);
+            btn_On7.TabIndex = 6;
+            btn_On7.Text = "On";
+            btn_On7.UseVisualStyleBackColor = true;
+            // 
+            // btn_On6
+            // 
+            btn_On6.Location = new Point(574, 6);
+            btn_On6.Name = "btn_On6";
+            btn_On6.Size = new Size(75, 116);
+            btn_On6.TabIndex = 5;
+            btn_On6.Text = "On";
+            btn_On6.UseVisualStyleBackColor = true;
+            // 
+            // btn_On5
+            // 
+            btn_On5.Location = new Point(461, 6);
+            btn_On5.Name = "btn_On5";
+            btn_On5.Size = new Size(75, 116);
+            btn_On5.TabIndex = 4;
+            btn_On5.Text = "On";
+            btn_On5.UseVisualStyleBackColor = true;
+            // 
+            // btn_On4
+            // 
+            btn_On4.Location = new Point(348, 6);
+            btn_On4.Name = "btn_On4";
+            btn_On4.Size = new Size(75, 116);
+            btn_On4.TabIndex = 3;
+            btn_On4.Text = "On";
+            btn_On4.UseVisualStyleBackColor = true;
+            // 
+            // btn_On3
+            // 
+            btn_On3.Location = new Point(235, 6);
+            btn_On3.Name = "btn_On3";
+            btn_On3.Size = new Size(75, 116);
+            btn_On3.TabIndex = 2;
+            btn_On3.Text = "On";
+            btn_On3.UseVisualStyleBackColor = true;
+            // 
+            // btn_On2
+            // 
+            btn_On2.Location = new Point(122, 6);
+            btn_On2.Name = "btn_On2";
+            btn_On2.Size = new Size(75, 116);
+            btn_On2.TabIndex = 1;
+            btn_On2.Text = "On";
+            btn_On2.UseVisualStyleBackColor = true;
+            // 
+            // btn_On1
+            // 
+            btn_On1.Location = new Point(9, 6);
+            btn_On1.Name = "btn_On1";
+            btn_On1.Size = new Size(75, 116);
+            btn_On1.TabIndex = 0;
+            btn_On1.Text = "On";
+            btn_On1.UseVisualStyleBackColor = true;
+            // 
+            // panel11
+            // 
+            panel11.Controls.Add(btn_Redo);
+            panel11.Dock = DockStyle.Right;
+            panel11.Location = new Point(850, 0);
+            panel11.Name = "panel11";
+            panel11.Size = new Size(80, 248);
+            panel11.TabIndex = 1;
+            // 
+            // btn_Redo
+            // 
+            btn_Redo.Dock = DockStyle.Fill;
+            btn_Redo.Location = new Point(0, 0);
+            btn_Redo.Name = "btn_Redo";
+            btn_Redo.Size = new Size(80, 248);
+            btn_Redo.TabIndex = 0;
+            btn_Redo.Text = "Redo";
+            btn_Redo.UseVisualStyleBackColor = true;
+            // 
+            // panel10
+            // 
+            panel10.Controls.Add(btn_Undo);
+            panel10.Location = new Point(0, 0);
+            panel10.Name = "panel10";
+            panel10.Size = new Size(80, 248);
+            panel10.TabIndex = 0;
+            // 
+            // btn_Undo
+            // 
+            btn_Undo.Dock = DockStyle.Fill;
+            btn_Undo.Location = new Point(0, 0);
+            btn_Undo.Name = "btn_Undo";
+            btn_Undo.Size = new Size(80, 248);
+            btn_Undo.TabIndex = 0;
+            btn_Undo.Text = "Undo";
+            btn_Undo.UseVisualStyleBackColor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { lightToolStripMenuItem, fanToolStripMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(102, 48);
+            // 
+            // lightToolStripMenuItem
+            // 
+            lightToolStripMenuItem.Name = "lightToolStripMenuItem";
+            lightToolStripMenuItem.Size = new Size(101, 22);
+            lightToolStripMenuItem.Text = "Light";
+            // 
+            // fanToolStripMenuItem
+            // 
+            fanToolStripMenuItem.Name = "fanToolStripMenuItem";
+            fanToolStripMenuItem.Size = new Size(101, 22);
+            fanToolStripMenuItem.Text = "Fan";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(930, 488);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel13.ResumeLayout(false);
+            panel12.ResumeLayout(false);
+            panel11.ResumeLayout(false);
+            panel10.ResumeLayout(false);
+            contextMenuStrip1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel9;
+        private Panel panel8;
+        private Panel panel7;
+        private Panel panel6;
+        private Panel panel5;
+        private Panel panel4;
+        private Panel panel3;
+        private Panel panel2;
+        private Panel panel13;
+        private Panel panel12;
+        private Panel panel11;
+        private Panel panel10;
+        private Button btn_Undo;
+        private Button btn_Off7;
+        private Button btn_Off4;
+        private Button btn_Off6;
+        private Button btn_Off1;
+        private Button btn_Off5;
+        private Button btn_Off2;
+        private Button btn_Off3;
+        private Button btn_On7;
+        private Button btn_On6;
+        private Button btn_On5;
+        private Button btn_On4;
+        private Button btn_On3;
+        private Button btn_On2;
+        private Button btn_On1;
+        private Button btn_Redo;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem lightToolStripMenuItem;
+        private ToolStripMenuItem fanToolStripMenuItem;
+    }
+}
